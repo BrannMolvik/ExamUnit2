1716dd97f6321e76f8d49446349b6c508a736f4d
BrannMolvik <brannmolvik@gmail.com>
2024-02-21 13:27:26 +0100

    test

--- template/Program.cs
+++ template/Program.cs
@@ -19,9 +19,75 @@ HttpUtils httpUtils = HttpUtils.instance;
 // We start by registering and getting the first task
 Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
 Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
-string taskID = ""; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+string taskID = "aaa"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
 
 //#### FIRST TASK 
 // Fetch the details of the task from the server.
 Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task1 = JsonSerializer.Deserialize<Task>(task1Response.content);
 Console.WriteLine(task1Response);
+
+
+
+string[] input = task1.parameters.Split(",");
+int answer = Add(input);
+
+static int Add(string[] numbers)
+    {
+    int sum = 0;
+    foreach (string number in numbers)
+    {
+        sum += int.Parse(number.Trim());
+    }
+
+    return sum;
+}
+Console.WriteLine(answer);
+Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer.ToString());
+Console.WriteLine($"Answer: {Colors.Green}{task1AnswerResponse}{ANSICodes.Reset}");
+
+taskID = "bbb"; 
+
+
+Console.WriteLine("\n-----------------------------------\n");
+
+Response task2Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task2 = JsonSerializer.Deserialize<Task>(task2Response.content);
+
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task2?.title}{ANSICodes.Reset}\n{task2?.description}\nParameters: {Colors.Yellow}{task2?.parameters}{ANSICodes.Reset}");
+
+string answer2 = "";
+
+
+if (task2.parameters.Contains("ice cream"))
+{
+    answer2 = "I scream for ice cream";
+}
+else
+{
+    answer2 = "I will have a cup of tea";
+}
+Console.WriteLine(answer2);
+Response task2AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answer2);
+Console.WriteLine($"\nAnswer: {Colors.Green}{task2AnswerResponse}{ANSICodes.Reset}");
+
+taskID = "ccc"; 
+
+
+Console.WriteLine("\n-----------------------------------\n");
+
+Response task3Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+Task task3 = JsonSerializer.Deserialize<Task>(task3Response.content);
+
+Console.WriteLine($"TASK: {ANSICodes.Effects.Bold}{task3?.title}{ANSICodes.Reset}\n{task3?.description}\nParameters: {Colors.Yellow}{task3?.parameters}{ANSICodes.Reset}");
+
+
+
+class Task
+{
+    public string? title { get; set; }
+    public string? description { get; set; }
+    public string? taskID { get; set; }
+    public string? usierID { get; set; }
+    public string? parameters { get; set; }
+}
